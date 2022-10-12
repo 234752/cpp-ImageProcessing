6625ca98ea252dd861620f7ca625f9128da61056
Patryk <patryk.gielcz@gmail.com>
2022-10-12 11:11:17 +0200

    other flips

--- ImageProcessor/Task1G.cpp
+++ ImageProcessor/Task1G.cpp
@@ -10,9 +10,41 @@ void horizontalFlip(CImg<unsigned char> &image)
     {
         for (int y = 0; y < image.height(); y++)
         {
-            flippedImage(image.width() - x, y, 0) = image(x, y, 0);
-            flippedImage(image.width() - x, y, 1) = image(x, y, 1);
-            flippedImage(image.width() - x, y, 2) = image(x, y, 2);
+            flippedImage(x, y, 0) = image(image.width() - x, y, 0);
+            flippedImage(x, y, 1) = image(image.width() - x, y, 1);
+            flippedImage(x, y, 2) = image(image.width() - x, y, 2);
+        }
+    }
+    image = flippedImage;
+}
+
+void verticalFlip(CImg<unsigned char> &image)
+{
+    CImg<unsigned char> flippedImage = image;
+
+    for (int x = 0; x < image.width(); x++)
+    {
+        for (int y = 0; y < image.height(); y++)
+        {
+            flippedImage(x, y, 0) = image(x, image.height() - y, 0);
+            flippedImage(x, y, 1) = image(x, image.height() - y, 1);
+            flippedImage(x, y, 2) = image(x, image.height() - y, 2);
+        }
+    }
+    image = flippedImage;
+}
+
+void diagonalFlip(CImg<unsigned char> &image)
+{
+    CImg<unsigned char> flippedImage = image;
+
+    for (int x = 0; x < image.width(); x++)
+    {
+        for (int y = 0; y < image.height(); y++)
+        {
+            flippedImage(x, y, 0) = image(image.width() - x, image.height() - y, 0);
+            flippedImage(x, y, 1) = image(image.width() - x, image.height() - y, 1);
+            flippedImage(x, y, 2) = image(image.width() - x, image.height() - y, 2);
         }
     }
     image = flippedImage;
--- ImageProcessor/main.cpp
+++ ImageProcessor/main.cpp
@@ -5,7 +5,7 @@
 int main() {
     cimg_library::CImg<unsigned char> image("..\\..\\img\\color_24bit\\lenac_small.bmp");
 
-    horizontalFlip(image);
+    diagonalFlip(image);
 
     image.save_bmp("..\\..\\img\\color_24bit\\out.bmp");
     return 0;
