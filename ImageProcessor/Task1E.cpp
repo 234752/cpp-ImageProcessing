62aa3ff031d340002152b1402ca5dff3818784be
Kacper Królewiak <kacperkrolewiak@protonmail.com>
2022-10-14 14:15:25 +0200

    Task 1E

--- ImageProcessor/Task1E.cpp
+++ ImageProcessor/Task1E.cpp
@@ -11,3 +11,53 @@ float meanSquareError(CImg<unsigned char> &image1, CImg<unsigned char> &image2) 
     }
     return e/(image1.height()*image2.width());
 }
+
+float peakMeanSquareError(CImg<unsigned char> &image1, CImg<unsigned char> &image2) {
+    float e = 0;
+    float m = 0;
+    for (unsigned int x = 0; x < image1.width(); x++) {
+        for (unsigned int y = 0; y < image1.height(); y++) {
+            if(image1(x,y) > m) m = image1(x,y);
+        }
+    }
+    for (unsigned int x = 0; x < image1.width(); x++) {
+        for (unsigned int y = 0; y < image1.height(); y++) {
+            e += pow((image1(x,y) - image2(x,y)), 2);
+        }
+    }
+    return e/(image1.height()*image2.width()); //idk where to divide by m
+}
+
+float signalToNoiseRatio(CImg<unsigned char> &image1, CImg<unsigned char> &image2) {
+    float signal = 0;
+    float noise = 0;
+    for (unsigned int x = 0; x < image1.width(); x++) {
+        for (unsigned int y = 0; y < image1.height(); y++) {
+            signal += pow((image1(x,y)), 2);
+            noise += pow((image1(x,y) - image2(x,y)), 2);
+        }
+    }
+    return 10*log10(signal/noise);
+}
+
+float peakSignalToNoiseRatio(CImg<unsigned char> &image1, CImg<unsigned char> &image2) {
+    float signal = 0;
+    float noise = 0;
+    for (unsigned int x = 0; x < image1.width(); x++) {
+        for (unsigned int y = 0; y < image1.height(); y++) {
+            if(image1(x,y) > signal) signal = image1(x,y);
+            noise += pow((image1(x,y) - image2(x,y)), 2);
+        }
+    }
+    return 10*log10((image1.width()*image1.height()*pow(signal,2))/noise);
+}
+
+float maxDifference(CImg<unsigned char> &image1, CImg<unsigned char> &image2) {
+    float m = 0;
+    for (unsigned int x = 0; x < image1.width(); x++) {
+        for (unsigned int y = 0; y < image1.height(); y++) {
+            if (std::abs(image1(x, y) - image2(x, y)) > m) m = std::abs(image1(x, y) - image2(x, y));
+        }
+    }
+    return m;
+}
--- ImageProcessor/main.cpp
+++ ImageProcessor/main.cpp
@@ -6,13 +6,14 @@
 
 int main() {
     cimg_library::CImg<unsigned char> image("..\\..\\img\\color_24bit\\lenac_small.bmp");
-    cimg_library::CImg<unsigned char> image_noise("..\\..\\img\\color_noise_24bit\\impulse\\lenac_impulse3_small.bmp");
+    cimg_library::CImg<unsigned char> image_low_noise("..\\..\\img\\color_noise_24bit\\impulse\\lenac_impulse1_small.bmp");
+    cimg_library::CImg<unsigned char> image_high_noise("..\\..\\img\\color_noise_24bit\\impulse\\lenac_impulse3_small.bmp");
     //cimg_library::CImg<unsigned char> image("..\\..\\img\\gray_scale_8bit\\lena_small.bmp");
 
     //contrast(image, 180);
 
-    std::cout << meanSquareError(image, image_noise);
-
+    std::cout << "\nLow noise: " << peakSignalToNoiseRatio(image, image_low_noise);
+    std::cout << "\nHigh noise: " << peakSignalToNoiseRatio(image, image_high_noise);
     //image.save_bmp("..\\..\\img\\color_24bit\\out.bmp");
     return 0;
 }
