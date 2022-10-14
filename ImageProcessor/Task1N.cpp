cb314abb04123ed0b29f6be2d1fc9516ba493b49
Patryk <patryk.gielcz@gmail.com>
2022-10-14 23:29:03 +0200

    refactoring + filter for all channels

--- ImageProcessor/Task1N.cpp
+++ ImageProcessor/Task1N.cpp
@@ -1,6 +1,5 @@
 #include "CImg.h"
 #include <vector>
-#include <algorithm>
 
 using namespace cimg_library;
 
@@ -24,7 +23,6 @@ int stageA(int zxy, int zmed, int zmin, int zmax, int Sxy, int Smax)
     if(A1 > 0 && A2 < 0) return stageB(zxy, zmed, zmin, zmax);
     else
     {
-        Sxy++;
         if(Sxy <= Smax) return 1000;
         else
         {
@@ -33,6 +31,45 @@ int stageA(int zxy, int zmed, int zmin, int zmax, int Sxy, int Smax)
     }
 }
 
+int filterSinglePixel(CImg<unsigned char> image, int x, int y, int channel)
+{
+    int Smax = image.width() - x;
+    if(image.height() - y < Smax) Smax = image.height() - y;
+    if(x < Smax) Smax = x;
+    if(y < Smax) Smax = y;
+    int Sxy = 1;
+
+    int result = 1000;
+    while(result == 1000)
+    {
+        std::vector<int> values;
+        for (int xi = x - Sxy; xi <= x + Sxy; xi++)
+        {
+            for (int yi = y - Sxy; yi <= y + Sxy; yi++)
+            {
+                if(xi != x || yi != y) values.push_back(image(xi, yi, channel));
+            }
+        }
+
+        std::sort(values.begin(), values.end());
+        int zmin = values.front();
+        int zmax = values.back();
+        int zxy = image(x, y, 0);
+        int zmed;
+        if(values.size()%2 == 0)
+        {
+            zmed = 0.5 * (values[values.size() / 2 - 1] + values[values.size() / 2]);
+        }
+        else
+        {
+            zmed = values[values.size() / 2];
+        }
+
+        result = stageA(zxy, zmed, zmin, zmax, Sxy, Smax);
+        if(result == 1000) Sxy++;
+    }
+    return result;
+}
 
 void adaptiveMedianFilter(CImg<unsigned char> &image)
 {
@@ -41,47 +78,9 @@ void adaptiveMedianFilter(CImg<unsigned char> &image)
     {
         for (int y = 1; y < image.height()-1; y++)
         {
-            int Smax = image.width() - x;
-            if(image.height() - y < Smax) Smax = image.height() - y;
-            if(x < Smax) Smax = x;
-            if(y < Smax) Smax = y;
-            int Sxy = 1;
-
-            int result = 1000;
-            while(true)
-            {
-                int zmin = 255;
-                int zmax = 0;
-                int zmed;
-                int zxy = image(x, y, 0);
-
-                std::vector<int> values;
-
-                for (int xi = x - Sxy; xi <= x + Sxy; xi++)
-                {
-                    for (int yi = y - Sxy; yi <= y + Sxy; yi++)
-                    {
-                        if(xi != x || yi != y) values.push_back(image(xi, yi, 0));
-                    }
-                }
-                std::sort(values.begin(), values.end());
-                zmin = values.front();
-                zmax = values.back();
-                if(values.size()%2 == 0)
-                {
-                    zmed = 0.5 * (values[values.size() / 2 - 1] + values[values.size() / 2]);
-                }
-                else
-                {
-                    zmed = values[values.size() / 2];
-                }
-                result = stageA(zxy, zmed, zmin, zmax, Sxy, Smax);
-                if(result == 1000) Sxy++;
-                else break;
-            }
-            filteredImage(x, y, 0) = result;
-            filteredImage(x, y, 1) = result;
-            filteredImage(x, y, 2) = result;
+            filteredImage(x, y, 0) = filterSinglePixel(image, x, y, 0);
+            filteredImage(x, y, 1) = filterSinglePixel(image, x, y, 1);
+            filteredImage(x, y, 2) = filterSinglePixel(image, x, y, 2);
         }
     }
     image = filteredImage;
--- ImageProcessor/main.cpp
+++ ImageProcessor/main.cpp
@@ -7,13 +7,13 @@
 
 int main() {
 
-    cimg_library::CImg<unsigned char> image("..\\..\\img\\gray_scale_noise_8bit\\uniform_dist\\lena_uniform3_small.bmp");
+    cimg_library::CImg<unsigned char> image("..\\..\\img\\color_noise_24bit\\uniform_dist\\lenac_uniform3_small.bmp");
     adaptiveMedianFilter(image);
-    image.save_bmp("..\\..\\img\\gray_scale_noise_8bit\\uniform_dist\\out.bmp");
+    image.save_bmp("..\\..\\img\\color_noise_24bit\\uniform_dist\\out.bmp");
 
-    cimg_library::CImg<unsigned char> noise_image("..\\..\\img\\gray_scale_noise_8bit\\uniform_dist\\lena_uniform3_small.bmp");
-    cimg_library::CImg<unsigned char> improved_image("..\\..\\img\\gray_scale_noise_8bit\\uniform_dist\\out.bmp");
-    cimg_library::CImg<unsigned char> original_image("..\\..\\img\\gray_scale_8bit\\lena_small.bmp");
+    cimg_library::CImg<unsigned char> noise_image("..\\..\\img\\color_noise_24bit\\uniform_dist\\lenac_uniform3_small.bmp");
+    cimg_library::CImg<unsigned char> improved_image("..\\..\\img\\color_noise_24bit\\uniform_dist\\out.bmp");
+    cimg_library::CImg<unsigned char> original_image("..\\..\\img\\color_24bit\\lenac_small.bmp");
 
     std::cout << "\nError before noise removal: " << meanSquareError(original_image, noise_image);
     std::cout << "\nError after noise removal: " << meanSquareError(original_image, improved_image);
