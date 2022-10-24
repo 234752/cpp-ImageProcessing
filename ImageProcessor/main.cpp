cb944a06696e910376069d39e9e09311f12f14e3
Patryk <patryk.gielcz@gmail.com>
2022-10-24 23:36:17 +0200

    triggers of CLI functions

--- ImageProcessor/main.cpp
+++ ImageProcessor/main.cpp
@@ -8,14 +8,12 @@
 
 
 using namespace popl;
+using namespace std;
 
-int main(int argc, char *argv[]) {
-
+int main(int argc, char *argv[])
+{
     OptionParser op("Allowed options");
     auto help_option     = op.add<Switch>("", "help", "produce help message");
-
-    //input image
-    auto input_option = op.add<Value<std::string>>("", "input-image", "provide name or path to input image [obligatory in case of all operations]", "");
 
     //B options
     auto brightness_option = op.add<Value<int>>("", "brightness", "brightness modification", 0);
@@ -35,7 +33,6 @@ int main(int argc, char *argv[]) {
     auto max_option = op.add<Switch>("", "max", "filter image using max filter");
 
     //E options
-    auto original_option = op.add<Value<std::string>>("", "original-image", "provide name or path to original image [obligatory in case of all error calculations]", "");
     auto mse_option = op.add<Switch>("", "mse", "calculate mean square error");
     auto pmse_option = op.add<Switch>("", "pmse", "calculate peak mean square error");
     auto snr_option = op.add<Switch>("", "snr", "calculate signal to noise ratio");
@@ -53,13 +50,55 @@ int main(int argc, char *argv[]) {
     // show unknown options (undefined ones, like "-u" or "--undefined")
     for (const auto& unknown_option: op.unknown_options()) std::cout << "unknown_options: " << unknown_option << "\n";
 
-    if(input_option->is_set())
+    CImg<unsigned char> inputImage;
+    CImg<unsigned char> originalImage;
+    const char *const input_image_path = argv[1];
+    const char *const original_image_path = argv[2];
+    try
     {
-        if(original_option->is_set())
+        inputImage = CImg(input_image_path);
+    }
+    catch (CImgIOException ex)
+    {
+        cout<<"input file does not exist, remember that first argument is name/path of input file";
+    }
+    if(mse_option->is_set() || pmse_option->is_set() || snr_option->is_set() || psnr_option->is_set() || md_option->is_set())
+    {
+        try
         {
-            std::cout<<input_option->value()<<"  "<<original_option->value();
+            originalImage = CImg(original_image_path);
+        }
+        catch (CImgIOException ex)
+        {
+            cout<<"original image file does not exist, remember that in case of error calculations second argument is name/path of original image";
         }
     }
+
+    //B options execution
+    if(brightness_option->is_set()) brightnessModification(inputImage, brightness_option->value());
+    if(contrast_option->is_set()) contrast(inputImage, contrast_option->value());
+    if(negative_option->is_set()) negative(inputImage);
+
+    //G options execution
+    if(hflip_option->is_set()) horizontalFlip(inputImage);
+    if(vflip_option->is_set()) verticalFlip(inputImage);
+    if(dflip_option->is_set()) diagonalFlip(inputImage);
+    if(shrink_option->is_set()) scale(inputImage, 1.0/shrink_option->value());
+    if(enlarge_option->is_set()) scale(inputImage, enlarge_option->value());
+
+    //N options execution
+    if(adaptive_option->is_set()) adaptiveMedianFilter(inputImage);
+    if(min_option->is_set()) minFilter(inputImage);
+    if(max_option->is_set()) maxFilter(inputImage);
+
+    //E options execution
+    if(mse_option->is_set()) cout<<"Mean square error: "<<meanSquareError(inputImage, originalImage)<<endl;
+    if(pmse_option->is_set()) cout<<"peak mean square error: "<<peakMeanSquareError(inputImage, originalImage)<<endl;
+    if(snr_option->is_set()) cout<<"Signal to noise ratio: "<<signalToNoiseRatio(inputImage, originalImage)<<endl;
+    if(psnr_option->is_set()) cout<<"Peak signal to noise ratio: "<<peakSignalToNoiseRatio(inputImage, originalImage)<<endl;
+    if(md_option->is_set()) cout<<"Max difference: "<<maxDifference(inputImage, originalImage)<<endl;
+
+    inputImage.save("..\\..\\img\\test\\out.bmp");
 
     return 0;
 }
