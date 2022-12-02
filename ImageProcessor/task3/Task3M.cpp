faa4865fcb960a7ab125b36bdcf94a60fdc0b672
Patryk <patryk.gielcz@gmail.com>
2022-12-02 20:08:31 +0100

    dilation fixed

--- ImageProcessor/task3/Task3M.cpp
+++ ImageProcessor/task3/Task3M.cpp
@@ -2,34 +2,41 @@
 
 using namespace cimg_library;
 
+short mask[3][3] =
+        {{0, 1, 0},
+         {1, 1, 1},
+         {0, 1, 0}};
+
+int findMaxValueInMask(CImg<unsigned char> &image, int x, int y)
+{
+    int max =0;
+    for(int maskX = -1; maskX<2; maskX++)
+    {
+        for(int maskY = -1; maskY<2; maskY++)
+        {
+            if(mask[maskX+1][maskY+1] == 1 && image(x + maskX , y + maskY , 0) > max)
+            {
+                max = image(x + maskX , y + maskY , 0);
+            }
+        }
+    }
+    return max;
+}
+
 void dilation(CImg<unsigned char> &image) {
 
-    short mask[3][3] =
-            {{0, 1, 0},
-             {1, 1, 1},
-             {0, 1, 0}};
+
     CImg<unsigned char> newImage = image;
 
     for (int x = 1; x < image.width()-1; x++)
     {
         for (int y = 1; y < image.height()-1; y++)
         {
-            if(image(x, y, 0) == 0)
-            {
-                int topLeftCornerX = x-1;
-                int topLeftCornerY = y-1;
-                for(int xi = 0; xi<3; xi++)
-                {
-                    for(int yi = 0; yi<3; yi++)
-                    {
-                        newImage(topLeftCornerX + xi, topLeftCornerY + yi, 0) =  newImage(topLeftCornerX + xi, topLeftCornerY + yi, 0) || mask[xi][yi];
-                        newImage(topLeftCornerX + xi, topLeftCornerY + yi, 1) =  newImage(topLeftCornerX + xi, topLeftCornerY + yi, 1) || mask[xi][yi];
-                        newImage(topLeftCornerX + xi, topLeftCornerY + yi, 2) =  newImage(topLeftCornerX + xi, topLeftCornerY + yi, 2) || mask[xi][yi];
-                    }
-                }
-            }
+            int newValue = findMaxValueInMask(image, x, y);
+            newImage(x, y, 0) = newValue;
+            newImage(x, y, 1) = newValue;
+            newImage(x, y, 2) = newValue;
         }
     }
-
     image = newImage;
 }
