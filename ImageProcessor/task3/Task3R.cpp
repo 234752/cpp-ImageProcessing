2a2ae5a57a684df7ae539055e4744f9f1622b821
Patryk <patryk.gielcz@gmail.com>
2022-12-14 00:41:56 +0100

    bruh moment

--- ImageProcessor/task3/Task3R.cpp
+++ ImageProcessor/task3/Task3R.cpp
@@ -2,7 +2,16 @@
 
 using namespace cimg_library;
 
-
+struct XY
+{
+    int x;
+    int y;
+    XY(int _x, int _y)
+    {
+        x = _x;
+        y = _y;
+    }
+};
 
 bool checkPixelsAround(CImg<unsigned char> image, CImg<unsigned char> region, int x, int y, int allowedDiff)
 {
@@ -13,59 +22,117 @@ bool checkPixelsAround(CImg<unsigned char> image, CImg<unsigned char> region, in
     return false;
 }
 
+bool checkSinglePixel(CImg<unsigned char> &image, int x, int y)
+{
+
+}
+
 void regionMerge(CImg<unsigned char> &image)
 {
 
     CImg<unsigned char> region(image.width(), image.height(), 1, 3, 0);
     int seedX = 470;
     int seedY = 180;
-    int allowedDiff = 10;
-    region(seedX, seedY, 0) = 255;
+    int allowedDiff = 200;
+    region(seedX, seedY, 0) = 1;
+
+    std::vector<XY> border = std::vector<XY>();
 
 
+    border.push_back(XY(seedX, seedY));
 
-    for (int x = 1; x < image.width()-1; x++)
-    {
-        for (int y = 1; y < image.height()-1; y++)
-        {
-            if(region(x,y,0) == 0 && checkPixelsAround(image, region, x, y, allowedDiff))
-            {
-                region(x,y,0) = 255;
+    while(true) {
+
+        bool anythingChanged = false;
+        std::vector<XY> newBorder = std::vector<XY>();
+
+
+        for (int i = 0; i < border.size(); i++) {
+
+
+            if(border[i].x>0 && border[i].x<image.width() && border[i].y>0 && border[i].y<image.height()) {
+
+
+                if (region(border[i].x + 1, border[i].y, 0) == 0 &&
+                    abs(image(border[i].x + 1, border[i].y, 0) - image(border[i].x, border[i].y, 0)) < allowedDiff) {
+                    newBorder.push_back(XY(border[i].x + 1, border[i].y));
+                    region(border[i].x + 1, border[i].y, 0) = 255;
+                    anythingChanged = true;
+                }
+
+                if (region(border[i].x - 1, border[i].y, 0) == 0 &&
+                    abs(image(border[i].x - 1, border[i].y, 0) - image(border[i].x, border[i].y, 0)) < allowedDiff) {
+                    newBorder.push_back(XY(border[i].x - 1, border[i].y));
+                    region(border[i].x - 1, border[i].y, 0) = 255;
+                    anythingChanged = true;
+                }
+
+                if (region(border[i].x, border[i].y + 1, 0) == 0 &&
+                    abs(image(border[i].x, border[i].y + 1, 0) - image(border[i].x, border[i].y, 0)) < allowedDiff) {
+                    newBorder.push_back(XY(border[i].x, border[i].y + 1));
+                    region(border[i].x, border[i].y + 1, 0) = 255;
+                    anythingChanged = true;
+                }
+
+                if (region(border[i].x, border[i].y - 1, 0) == 0 &&
+                    abs(image(border[i].x, border[i].y - 1, 0) - image(border[i].x, border[i].y, 0)) < allowedDiff) {
+                    newBorder.push_back(XY(border[i].x, border[i].y - 1));
+                    region(border[i].x, border[i].y - 1, 0) = 255;
+                    anythingChanged = true;
+                }
             }
         }
+
+        border = newBorder;
+        if(!anythingChanged) break;
+
+
     }
 
-    for (int x = image.width()-1; x > 1; x--)
-    {
-        for (int y = 1; y < image.height()-1; y++)
-        {
-            if(region(x,y,0) == 0 && checkPixelsAround(image, region, x, y, allowedDiff))
-            {
-                region(x,y,0) = 255;
-            }
-        }
-    }
 
-    for (int x = 1; x < image.width()-1; x++)
-    {
-        for (int y = image.height()-1; y > 1; y--)
-        {
-            if(region(x,y,0) == 0 && checkPixelsAround(image, region, x, y, allowedDiff))
-            {
-                region(x,y,0) = 255;
-            }
-        }
-    }
 
-    for (int x = image.width()-1; x > 1; x--)
-    {
-        for (int y = image.height()-1; y > 1; y--)
-        {
-            if(region(x,y,0) == 0 && checkPixelsAround(image, region, x, y, allowedDiff))
-            {
-                region(x,y,0) = 255;
-            }
-        }
-    }
+//    for (int x = 1; x < image.width()-1; x++)
+//    {
+//        for (int y = 1; y < image.height()-1; y++)
+//        {
+//            if(region(x,y,0) == 0 && checkPixelsAround(image, region, x, y, allowedDiff))
+//            {
+//                region(x,y,0) = 255;
+//            }
+//        }
+//    }
+//
+//    for (int x = image.width()-1; x > 1; x--)
+//    {
+//        for (int y = 1; y < image.height()-1; y++)
+//        {
+//            if(region(x,y,0) == 0 && checkPixelsAround(image, region, x, y, allowedDiff))
+//            {
+//                region(x,y,0) = 255;
+//            }
+//        }
+//    }
+//
+//    for (int x = 1; x < image.width()-1; x++)
+//    {
+//        for (int y = image.height()-1; y > 1; y--)
+//        {
+//            if(region(x,y,0) == 0 && checkPixelsAround(image, region, x, y, allowedDiff))
+//            {
+//                region(x,y,0) = 255;
+//            }
+//        }
+//    }
+//
+//    for (int x = image.width()-1; x > 1; x--)
+//    {
+//        for (int y = image.height()-1; y > 1; y--)
+//        {
+//            if(region(x,y,0) == 0 && checkPixelsAround(image, region, x, y, allowedDiff))
+//            {
+//                region(x,y,0) = 255;
+//            }
+//        }
+//    }
     image = region;
 }
