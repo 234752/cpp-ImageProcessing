80edfd0d18390a4c42d69f6c789354cb28423eaa
Patryk <patryk.gielcz@gmail.com>
2022-12-30 22:13:42 +0100

    FFT implemented

--- ImageProcessor/task4/Task4T.cpp
+++ ImageProcessor/task4/Task4T.cpp
@@ -41,9 +41,10 @@ vector<vector<complex<double>>> matrixDFT(vector<vector<complex<double>>> inputM
                 }
             }
 
-            cout<<u<<" "<<v<<" "<<sum.real()<<" "<<sum.imag()<<endl;
+            //cout<<u<<" "<<v<<" "<<sum.real()<<" "<<sum.imag()<<endl;
             outputMatrix[u][v] = sum;
         }
+        cout<<u<<endl;
     }
     return outputMatrix;
 }
@@ -65,7 +66,7 @@ void DFT(CImg<unsigned char> &image)
         }
     }
     vector<vector<complex<double>>> dft = matrixDFT(matrix, M, N, -1);
-    dft = matrixDFT(dft, M, N, 1);
+    //dft = matrixDFT(dft, M, N, 1);
 
     for(int i=0; i<M; i++)
     {
@@ -81,5 +82,100 @@ void DFT(CImg<unsigned char> &image)
     transformed.save("both_domains.bmp");
 }
 
-void FFT(CImg<unsigned char> &image) {}
+void linearFFT(vector<complex<double>> &x, int inverse) {
+
+    int n = x.size();
+
+    if (n == 1) {
+        return;
+    }
+
+    vector<complex<double>> even(n / 2);
+    vector<complex<double>> odd(n / 2);
+    for (int i = 0; i < n / 2; i++) {
+        even[i] = x[2 * i];
+        odd[i] = x[2 * i + 1];
+    }
+
+    linearFFT(even, inverse);
+    linearFFT(odd, inverse);
+
+    double angle = 2 * M_PI / n * (inverse ? -1 : 1);
+    complex<double> w(1);
+    complex<double> wn(cos(angle), sin(angle));
+    for (int i = 0; i < n / 2; i++) {
+        x[i] = even[i] + w * odd[i];
+        x[i + n / 2] = even[i] - w * odd[i];
+        w *= wn;
+    }
+}
+
+void matrixFFT(vector<vector<complex<double>>> &inputMatrix, int inverse) {
+    int rows = inputMatrix.size();
+    int cols = inputMatrix[0].size();
+
+    for (vector<complex<double>> &row : inputMatrix) {
+        linearFFT(row, inverse);
+    }
+
+    vector<vector<complex<double>>> transposedMatrix(cols, vector<complex<double>>(rows));
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            transposedMatrix[j][i] = inputMatrix[i][j];
+        }
+    }
+
+    for (vector<complex<double>> &col : transposedMatrix) {
+        linearFFT(col, inverse);
+    }
+
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < rows; j++) {
+            inputMatrix[j][i] = transposedMatrix[i][j];
+        }
+    }
+}
+
+void FFT(CImg<unsigned char> &image) {
+    CImg<unsigned char> transformed(image.width(), image.height(), 1, 3, 0);
+
+    int N = image.height();
+    vector<vector<complex<double>>> matrix(N, vector<complex<double>>(N));
+
+
+    for(int x=0; x < N; x++)
+    {
+        for(int y=0; y < N; y++)
+        {
+            matrix[x][y] = image(x,y,0);
+        }
+    }
+
+    matrixFFT(matrix, 0);
+
+
+    int maxValue = 0;
+    for(int i=0; i<N; i++)
+    {
+        for(int j=0; j<N; j++)
+        {
+            int value = log(1+ pow(pow(matrix[i][j].imag(),2) + pow(matrix[i][j].real(),2), 0.5));
+            if(value > maxValue) maxValue = value;
+        }
+    }
+    for(int i=0; i<N; i++)
+    {
+        for(int j=0; j<N; j++)
+        {
+            int value = log(1+ pow(pow(matrix[i][j].imag(),2) + pow(matrix[i][j].real(),2), 0.5));
+            value *= 255;
+            value /= maxValue;
+            transformed(i,j,0) = value;
+            transformed(i,j,1) = value;
+            transformed(i,j,2) = value;
+            std::cout<<i<<" "<<j<<"  "<<matrix[i][j].real()<<" "<<matrix[i][j].imag()<<"  "<<value<<endl;
+        }
+    }
+    transformed.save("FFT.bmp");
+}
 void IFFT(CImg<unsigned char> &image) {}
